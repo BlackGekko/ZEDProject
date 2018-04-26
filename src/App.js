87b5994dacc1d6aa70693bcc344de9b7bc208502
Tyler Wood <blackgekkotyler@outlook.com>
2018-04-25 17:43:09 -0700

    commenting, color test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Home return to top buttons
+//Animated home screen, reacts to mouse movement?
+//circle/squared images on display
+//may 18 personal dedline
+//Lauch static / back end development begins
+
+
 class App extends Component {
   render() {
     return (
