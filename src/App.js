5444166c9543c04c112d6b4e277bb7e37c75e410
jeramian <blackgekkotyler@outlook.com>
2018-04-26 03:02:46 -0700

    created header with animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import {
+//  Route,
+//  NavLink,
+//  HashRouter
+//} from "react-router-dom";
 
 //Home return to top buttons
 //Animated home screen, reacts to mouse movement?
@@ -9,20 +12,14 @@ import './App.css';
 //Lauch static / back end development begins
 
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      //<HashRouter>
+        <div>
+          
+        </div>
+      //</HashRouter>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './stylesheet.css';
+import './css/foundation.css';
+import './css/app.css';
+import './canvas.js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 registerServiceWorker();
