dea91b048102ff3803cbe3d70c1dd7b1f60fda0f
Jeramian <jeremyleramian@outlook.com>
2018-05-10 10:43:38 -0700

    Continuing production of albums page, after all images are added for their specified month, react dynamic page wil be created to show full albums

--- src/Albums.js
+++ src/Albums.js
@@ -11,7 +11,7 @@ import $ from 'jquery';
 import Footer from './Footer.js';
 import MarchOne from './MarchOne.js';
 
-//All albums will be laid out accroding to the date and year they were published, albums will descend new > old
+//All albums will be laid out accroding to the date and year they were published, albums will descend new -> old
 //Impliment JQUERY class access to find what image is being clicked to display the modal image -- IN PROGRESS
 
 export default class Albums extends React.Component {
@@ -25,18 +25,39 @@ export default class Albums extends React.Component {
                             <hr></hr>
                             <div className='sidebar'>
                                 <p>April, 2018</p>
+                                <hr></hr>
                             </div>
                             <div className='a2018'>
-                                <img className="myImg" id='ap1' data-featherlight={require('./images/April_2018/1.jpeg')} src={require('./images/April_2018/1.jpg')} alt="Photo Caption" />
-                                <img className="myImg" id='ap2' data-featherlight={require('./images/April_2018/2.jpeg')} src={require('./images/April_2018/2.jpg')} alt="Photo Caption" />
+                                <img className="myImg" id='ap1'src={require('./images/April_2018/1.jpg')} alt="Photo Caption" />
+                                <img className="myImg" id='ap2'src={require('./images/April_2018/2.jpg')} alt="Photo Caption" />
                             </div>
-                            <hr></hr>
+                            <br></br>
                             <div className='sidebar'>
                                 <p>March, 2018</p>
+                                <hr></hr>
                             </div>
                             <div className='mar2018'>
-                              <img className="myImg" id='marchOne' data-featherlight={require('./images/March_2018/march1.jpg')} src={require('./images/March_2018/march1sm.jpeg')} alt="Photo Caption" />
-                              <Link to='/marchOne'>{MarchOne}</Link>
+                              <img className="myImg" id='marchOne'src={require('./images/March_2018/march1sm.jpeg')} alt="Photo Caption" />
+                              <img className="myImg" id='marchTwo'src={require('./images/March_2018/March_8_2018/march2.jpg')} alt="Photo Caption" />
+                              <img className="myImg" id='marchThree'src={require('./images/March_2018/March_8_2018/march3sm.jpeg')} alt="Photo Caption" />
+                              <img className="myImg" id='marchFour'src={require('./images/March_2018/March_8_2018/march4sm.jpeg')} alt="Photo Caption" />
+                            </div>
+                            <div className='sidebar'>
+                              <p>February, 2018</p>
+                              <hr></hr>
+                            </div>
+                            <div className='feb2018'>
+                              <img className="myImg" id='Feb1' src={require('./images/March_2018/Feb_18_2018/feb1sm.jpeg')} alt="Photo Caption" />
+                              <img className="myImg" id='Feb2' src={require('./images/March_2018/Feb_18_2018/feb2sm.jpeg')} alt="Photo Caption" />
+                              <img className="myImg" id='Feb3' src={require('./images/March_2018/Feb_18_2018/feb3sm.jpeg')} alt="Photo Caption" />
+                              <img className="myImg" id='Feb4' src={require('./images/March_2018/Feb_18_2018/feb4sm.jpeg')} alt="Photo Caption" />
+                            </div>
+                            <div className='sidebar'>
+                              <p>January, 2018</p>
+                              <hr></hr>
+                            </div>
+                            <div className='jan2018'>
+
                             </div>
                             <hr></hr>
                         </div>
